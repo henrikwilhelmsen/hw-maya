2567a1da1d4734a45db82bf1b7b9f27e82637699
Henrik Wilhelmsen <contact@henrikwilhelmsen.com>
2025-07-09 21:07:54 +0200

    clean up basedpyright and ruff errors

--- scripts/maya/userSetup.py
+++ scripts/maya/userSetup.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 Henrik Wilhelmsen.
+# Copyright (C) 2025 Henrik Wilhelmsen.  # noqa: D100, INP001
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
--- src/hw_maya/constants.py
+++ src/hw_maya/constants.py
@@ -3,10 +3,7 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at <https://mozilla.org/MPL/2.0/>.
-
-from pathlib import Path
-
-DATA_DIR = Path(__file__).parent / "data"
+"""Package constants."""
 
 SKIN_CLUSTER_SUFFIX = "_skinCluster"
 NG_SKIN_PREFIX = "ngSkinToolsData_"
